b67e1f234967a85f5a9c91f6a84e789c401f67c6
lillyschulte <rickschulte@hotmail.nl>
2022-12-15 13:30:14 +0100

    First GUI rest

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 # Import required libraries
 import socket
 import subprocess
+import tkinter as tk
 
 # Set the network range to scan
 network = "192.168.1.0/24"
 
-# Use the nmap command to scan the network
-output = subprocess.run(["nmap", "-sn", network], capture_output=True)
+# Create the GUI window
+window = tk.Tk()
+window.title("Network Scanner")
 
-# Loop through the output lines
-for line in output.stdout.decode().splitlines():
-    # Check for lines containing IP addresses
-    if "Nmap scan report for" in line:
-        # Extract the IP address from the line
-        ip_address = line.split("for ")[1]
+# Create a text area to display the results
+text_area = tk.Text(window)
+text_area.pack(fill=tk.BOTH, expand=True)
 
-        # Print the IP address
-        print(ip_address)
+# Function to scan the network and display the results
+def scan_network():
+    # Use the nmap command to scan the network
+    output = subprocess.run(["nmap", "-sn", network], capture_output=True)
+
+    # Clear the text area
+    text_area.delete(1.0, tk.END)
+
+    # Loop through the output lines
+    for line in output.stdout.decode().splitlines():
+        # Check for lines containing IP addresses
+        if "Nmap scan report for" in line:
+            # Extract the IP address from the line
+            ip_address = line.split("for ")[1]
+
+            # Print the IP address to the text area
+            text_area.insert(tk.END, ip_address + "\n")
+
+# Create a button to start the scan
+scan_button = tk.Button(window, text="Scan Network", command=scan_network)
+scan_button.pack(fill=tk.X)
+
+# Start the GUI event loop
+window.mainloop()
